d141c7c6c6a861e77fd678f8788cddadfd21429e
mujugen <kaingame3@gmail.com>
2024-02-23 07:33:07 +0800

    NO PROFANITY

--- game/menu.py
+++ game/menu.py
@@ -1,26 +1,28 @@
 import pygame
 from game import WINDOW_WIDTH, WINDOW_HEIGHT
 
+
 class Menu:
 
     OPTIONS = ["TEST AI", "SOLO", "AI VS PLAYER", "Exit"]
-    TITLE = "FLAPPY TITE"
+    TITLE = "FLAPPY BIRD"
     SUBTITLE = "Choose a category:"
     INTRO = "Kain inom Gala Presents"
     SELECTED = 0
     run = True
+
     def __init__(self, win):
         self.win = win
         self.font = pygame.font.SysFont("comicsans", 50)
         self.run = True
         self.clock = pygame.time.Clock()
-    
+
     def draw(self):
-        self.win.fill((0,0,0)) # Fills the window with black
+        self.win.fill((0, 0, 0))  # Fills the window with black
         # Text related
-        title = self.font.render(self.TITLE, 1, (255,255,255))
-        subtitle = self.font.render(self.SUBTITLE, 0.3, (255,255,255))
-        intro = self.font.render(self.INTRO, 0.2, (255,255,255))
+        title = self.font.render(self.TITLE, 1, (255, 255, 255))
+        subtitle = self.font.render(self.SUBTITLE, 0.3, (255, 255, 255))
+        intro = self.font.render(self.INTRO, 0.2, (255, 255, 255))
         # Draws the title
         self.win.blit(title, (WINDOW_WIDTH/2 - title.get_width()/2, 80))
         self.win.blit(subtitle, (WINDOW_WIDTH/2 - subtitle.get_width()/2, 150))
@@ -28,11 +30,13 @@ class Menu:
         # Draws the options
         for i in range(len(self.OPTIONS)):
             if i == self.SELECTED:
-                text = self.font.render(self.OPTIONS[i], 1, (255,0,0))
+                text = self.font.render(self.OPTIONS[i], 1, (255, 0, 0))
             else:
-                text = self.font.render(self.OPTIONS[i], 1, (255,255,255))
-            self.win.blit(text, (WINDOW_WIDTH/2 - text.get_width()/2, 250 + i*50))
+                text = self.font.render(self.OPTIONS[i], 1, (255, 255, 255))
+            self.win.blit(
+                text, (WINDOW_WIDTH/2 - text.get_width()/2, 250 + i*50))
         pygame.display.update()
+
     def run_menu(self):
         """Runs the menu loop"""
         while self.run:
