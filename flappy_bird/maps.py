4bb9faf9f61df24983a2ecf9d998d3b86c439fcd
Johndel <johndelencabo@gmail.com>
2024-03-11 22:31:59 +0800

    New maps added

--- flappy_bird/maps.py
+++ flappy_bird/maps.py
@@ -36,9 +36,9 @@ class Map(MapAbstract):
     
     def load_assets(self):
         """Loads the assets of the map"""
-        self.pipes = [Pipe(600, pygame.transform.scale2x(pygame.image.load(os.path.join(MAP_ASSET_DIR, self.pipe))))]
-        self.floor = Floor(730, pygame.transform.scale2x(pygame.image.load(os.path.join(MAP_ASSET_DIR, self.floor))), velocity=self.PIPE_VEL)
-        self.bg = pygame.transform.scale2x(pygame.image.load(os.path.join(MAP_ASSET_DIR, self.bg)))
+        self.pipes = [Pipe(600, pygame.transform.scale(pygame.image.load(os.path.join(MAP_ASSET_DIR, self.pipe)), (52*2, 329*2)))]
+        self.floor = Floor(730, pygame.transform.scale(pygame.image.load(os.path.join(MAP_ASSET_DIR, self.floor)), (350*2,112*2.2)), velocity=self.PIPE_VEL)
+        self.bg = pygame.transform.scale(pygame.image.load(os.path.join(MAP_ASSET_DIR, self.bg)), (300 * 2, 515 *1.5))
     
 class MapHandler(Map):
     """MapHandler class will handle the random generation of the pipes"""
@@ -67,7 +67,7 @@ class MapHandler(Map):
         return self.new_map()
 
 # List here the maps that will be used in the game, inherit the Map class
-class Map1(Map):
+class MapDefault(Map):
     """Map1 class will handle the first map"""
     
     name = "Map1"
@@ -77,11 +77,47 @@ class Map1(Map):
     PIPE_VEL = 6
     PIPE = 300
 
-class Map2(Map):
+class MapFantasy(Map):
     """Map2 class will handle the second map"""
-    name = "Map2"
-    pipe = "default\\pipe.png"
+    name = "Fantasy"
+    pipe = "1\\pipe.png"
     floor = "default\\floor.png"
     bg = "1\\bg.png"
-    PIPE_VEL = 7
-    PIPE_GAP = 450
+    PIPE_VEL = 6
+    PIPE_GAP = 400
+
+class MapCity(Map):
+    """Map3 class will handle the second map"""
+    name = "City"
+    pipe = "2\\pipe.png"
+    floor = "2\\floor.png"
+    bg = "2\\bg.png"
+    PIPE_VEL = 6
+    PIPE_GAP = 400
+
+class MapSnow(Map):
+    """Map4 class will handle the second map"""
+    name = "Snow"
+    pipe = "3\\pipe.png"
+    floor = "3\\floor.png"
+    bg = "3\\bg.png"
+    PIPE_VEL = 6
+    PIPE_GAP = 400
+
+class MapNight(Map):
+    """Map5 class will handle the second map"""
+    name = "Night"
+    pipe = "4\\pipe.png"
+    floor = "4\\floor.png"
+    bg = "4\\bg.png"
+    PIPE_VEL = 6
+    PIPE_GAP = 400
+
+class MapWater(Map):
+    """Map6 class will handle the second map"""
+    name = "Water"
+    pipe = "5\\pipe.png"
+    floor = "5\\floor.png"
+    bg = "5\\bg.png"
+    PIPE_VEL = 6
+    PIPE_GAP = 400
