98a4a903541877c9da0f4b569c227032a5126a62
mujugen <kaingame3@gmail.com>
2024-03-23 09:39:06 +0800

    fixed mode reset upon return to menu

--- main.py
+++ main.py
@@ -12,15 +12,9 @@ from flappy_bird.maps import MapHandler
 
 def main():
     # Initiate the window
-    win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))    
-    # Initiate the menu
+    win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) 
     menu = Menu(win)
-    # This line of code from line 19 to 30 will insert into the menu class, for the map choice
-    # Intiate the intance of the Character
-    # char = Bird(230, 350)
-    # Initiate the base
-    # Initiate the Map
-    maps = MapHandler()   
+    
     # The game loop
     run = True # Run the game
     # Notice: This run variable is used to control the game loop
@@ -32,15 +26,16 @@ def main():
             menu.draw()
             menu.run_menu()    
             continue
-        # The pause of the game is not yet working or the game loop 
+
+        maps = MapHandler()   
         mode = Environment.modes()[menu.selected_mode](win, maps, menu.selected_character)
         mode.run()
-        
-        if mode.is_finished == True and mode.is_running == False:
-            mode.clear()
-            del mode
-            menu.run = True
-            continue
+        # clear() resets the character object instantiated in the mode
+        mode.clear()
+        # deletes both mode and map so a new map can be selected when going to back game mode
+        del mode, maps
+        menu.run = True
+        continue
 
         # if menu.selected_mode == 3:
         #     # Exit
